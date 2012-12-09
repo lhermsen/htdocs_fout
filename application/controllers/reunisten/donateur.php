b705f0af099b402c0c1a61bd9222380fa5bdebd1
lhermsen <lucashermsen@gmail.com>
2012-12-09 22:44:21 +0100

    Controller donateur worden reunistengedeelte

---
+++ application/controllers/reunisten/donateur.php
@@ -0,0 +1,30 @@
+﻿<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Donateur extends CI_Controller {
+
+	public function index()
+	{
+		// Laad de views
+		
+		$this->pagina->header();
+		$this->load->view('reunisten/nieuwsbox');
+		$this->load->view('reunisten/navigatie');
+		$this->load->view('reunisten/suggesties');
+		$this->pagina->footer();
+	}
+	
+	public function verwerk()
+	{
+		// Controleer of het formulier juist is ingevuld
+		if($this->form_validation->run())
+		{
+			// Creëer nieuwe donateur in de database en laat email sturen
+			$this->donateur_model->nieuw($_POST);
+		} 
+		else
+		{
+			// Als het formulier onjuist is ingevuld, toon dan weer het formulier
+			$this->index();
+		}
+	}
+}
